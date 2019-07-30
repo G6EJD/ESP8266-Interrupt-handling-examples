6060141e3a28fa9cfc85d1f6212bb424d6568d1d
G6EJD <d.l.bird@sky.com>
2019-07-30 10:45:30 +0100

    Update ESP8266_Interrupt_Example.ino

--- ESP8266_Interrupt_Example.ino
+++ ESP8266_Interrupt_Example.ino
@@ -3,7 +3,7 @@ Example Timer0 Interrupt
 Flash LED every second
 */
 
-#define ledPin LED_BUILTIN
+#define ledPin LED_BUILTIN // or define ledPin 5
 #define timer0_preload 40161290
 
 #define my_delay 2 // Set interrupt time in secs. Value = 2 x Number of Seconds, so 10-Secs = 10 x 2 = 20
@@ -13,13 +13,13 @@ volatile int toggle;
 
 void inline handler (void){
   toggle = (toggle == 1) ? 0 : 1;
-  digitalWrite(BUILTIN_LED, toggle);
+  digitalWrite(ledPin, toggle);
   timer0_write(ESP.getCycleCount() + timer0_preload * my_delay); // 
 }
  
 void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);
-  digitalWrite(BUILTIN_LED, HIGH); // Turn OFF LED to start with
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH); // Turn OFF LED to start with
   noInterrupts();
   timer0_isr_init();
   timer0_attachInterrupt(handler);
